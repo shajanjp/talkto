0626f06d9671a006d1f932c93e830a0c2d5d934a
Shajan Jacob <shajanjacob@wimoku.com>
2020-03-15 17:15:30 +0530

    refactor: better configuration control and custom host support

--- index.js
+++ index.js
@@ -9,47 +9,61 @@ let userConfig = {};
 const socketClient = require('socket.io-client')
 let socket;
 
-getConfig()
-.then(configData => {
-  userConfig = makeConfigDefaults(configData);
-  socket = socketClient(userConfig.host);
-})
-.catch(error => {})
-.finally(() => {
-  console.log("You are " + userConfig.username + '.'); 
-  socket.on(userConfig.username, function(data){
-    console.log(`\x1b[33m${data.fromAddress}: ${data.text}\x1b[0m`);
-  });
-  socket.on('connect', function(){
-    console.log('You are online.');
-  });
-})
+// argv 1 :  chat 
+if(argv._.length == 1 && argv._[0]){
+  chatInit();
+}
 
-if (argv.username && argv.username != '') {
-  userConfig.username = argv.username;
+// argv 1 :  setusername 
+if(argv.user && typeof argv.user == 'object') {
+  userConfig.username = argv.user.name;
   setConfig(userConfig)
   .finally(() => {
     process.exit();
   })
 }
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
+if(argv.host && argv.host !== '') {
+  getConfig()
+  .then(userConfig => {
+    userConfig.host = argv.host;
+    return setConfig(userConfig);
+  })
+  .finally(() => {
+    process.exit();
+  })
+}
 
-
-
-rl.on('line', function(line){
-  console.log('\033[1A' + `\x1b[32mYou: ${line}\x1b[0m`);
-  socket.emit('MESSAGE', { toAddress, fromAddress: userConfig.username, text: line });
-});
-
-
-if(argv._[0]){
+function chatInit(){
   toAddress = argv._[0];
   console.log('You are talking to', toAddress);
+
+  getConfig()
+  .then(configData => {
+    userConfig = makeConfigDefaults(configData);
+    socket = socketClient(userConfig.host);
+  })
+  .catch(error => {})
+  .finally(() => {
+    console.log("You are " + userConfig.username + '.'); 
+    socket.on(userConfig.username, function(data){
+      console.log(`\x1b[33m${data.fromAddress}: ${data.text}\x1b[0m`);
+    });
+    socket.on('connect', function(){
+      console.log('You are online.');
+    });
+  })
+
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  });
+
+  rl.on('line', function(line){
+    console.log('\033[1A' + `\x1b[32mYou: ${line}\x1b[0m`);
+    socket.emit('MESSAGE', { toAddress, fromAddress: userConfig.username, text: line });
+  });
 }
 
 function setConfig(data){
